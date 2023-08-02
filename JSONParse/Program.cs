7c9de2213d046861b28c388c8013638cd50ef9d8
Ahmet Gökhan Gürel <ahmet.gokhan.gurel@gmail.com>
2023-08-02 17:19:55 +0300

    Add project files.

---
+++ JSONParse/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader r = new StreamReader("C:\\response.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Receipt> items = JsonConvert.DeserializeObject<List<Receipt>>(json);
+
+                Console.WriteLine(items[0].description);
+   
+                Console.ReadKey();
+            }
+
+            
+        }
+    }
+}
---
+++ JSONParse/Receipt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONParse
+{
+    public class Receipt
+    {
+        public string locale { get; set; }
+        public string description { get; set; }
+        public BoundingPoly boundingPoly { get; set; }
+    }
+
+    public class BoundingPoly
+    {
+        public List<Vertex> vertices { get; set; }
+    }
+
+    public class Vertex
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
